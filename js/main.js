d104e8c8e136aba95673863964fc99d061f0d659
Contemplat0r <uldo.forme@gmail.com>
2021-02-10 22:02:47 +0200

    Experiments with window.location continue. Uncommented main.js and added (in main.js) window.location.href and console.log for show window.location.href

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 window.onload = () => {
   'use strict';
 
+  console.log("main.js, widnod.location.href: " + window.location.href);
+
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(window.location.href + '../../sw.js');
   }
